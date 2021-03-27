fd08e78f06949574a2fdebb1189b78a829179278
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-03-27 19:19:09 +0200

    Add partials import

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.css';
+import header from './partials/header.html';
+import footer from './partials/footer.html';
+
+const bodyRef = document.querySelector('BODY');
+
+bodyRef.insertAdjacentHTML('afterbegin', header);
+bodyRef.insertAdjacentHTML('beforeend', footer);
