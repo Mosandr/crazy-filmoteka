215e9fc31872cd1cbce42d92c075b6e45ee03cd7
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-03-28 17:54:28 +0300

    Add API settings file

---
+++ src/js/apiSettings.js
@@ -0,0 +1,8 @@
+// API Key (v3 auth)
+const apiKey = '62af7111fb0063930df9c7b53547034d';
+
+// Example API Request
+// https://api.themoviedb.org/3/movie/550?api_key=62af7111fb0063930df9c7b53547034d
+
+// API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmFmNzExMWZiMDA2MzkzMGRmOWM3YjUzNTQ3MDM0ZCIsInN1YiI6IjYwNjA5MjgxYjdiNjlkMDAyOGM0ZTk4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b8AQueAckiwD4aMem_LZS91KjvxpxZji6AAqu8NpVYY
