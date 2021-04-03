6780d810edabc2a4451dd17fabc204e62d7926e8
Andriy Mosol <40470899+Mosandr@users.noreply.github.com>
2021-04-03 15:45:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './sass/styles.scss';
 import UiService from './js/uiService';
-import './js/auth.js';
 import header from './partials/header.html';
 import footer from './partials/footer.html';
+import auth from './partials/authModal.html';
+import Server from './js/serviceDB.js';
+import Auth from './js/auth.js';
+import initModal from './js/initModal.js';
 import Preloader from './js/preloader-backdrop';
 
 const bodyRef = document.querySelector('BODY');
@@ -12,8 +15,23 @@ bodyRef.insertAdjacentHTML('beforeend', footer);
 bodyRef.insertAdjacentHTML('afterbegin', auth);
 
 const ui = new UiService();
+const server = new Server();
+const authorization = new Auth();
 
 ui.init();
 
-initFirebase();
 initModal();
+
+authorization.init();
+
+server.auth.onAuthStateChanged(user => {
+  if (user) {
+    authorization.setupUI(user);
+    server.renewQueueList(user);
+    server.renewWatchedList(user);
+    server.getActualQueueLists(user);
+    server.getActualWatchedLists(user);
+  } else {
+    authorization.setupUI();
+  }
+});
