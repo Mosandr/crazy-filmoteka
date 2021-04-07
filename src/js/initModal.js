b280791689edbef23e3618e17e3cbf1eebf5c1ad
Tetiana Proshyna <mollinne2@gmail.com>
2021-04-07 12:19:00 +0300

    modal Scroll try2

--- src/js/initModal.js
+++ src/js/initModal.js
@@ -3,7 +3,13 @@ import 'materialize-css/dist/js/materialize.min.js';
 export default function initModal() {
   document.addEventListener('DOMContentLoaded', function () {
     const elems = document.querySelectorAll('.modal');
-    M.Modal.init(elems);
+    const options = {
+      onCloseEnd: function () {
+        const over = document.querySelector('.over');
+        over.classList.remove('is-shown');
+      },
+    };
+    M.Modal.init(elems, options);
   });
 
   // close modal-login while opening modal-signup
@@ -12,5 +18,13 @@ export default function initModal() {
   signUpBtn.addEventListener('click', () => {
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
+  });
+
+  const modalTriger = document.querySelector('.gallery__list');
+
+  modalTriger.addEventListener('click', event => {
+    if (event.target.nodeName === 'UL') return;
+    const over = document.querySelector('.over');
+    over.classList.add('is-shown');
   });
 }
