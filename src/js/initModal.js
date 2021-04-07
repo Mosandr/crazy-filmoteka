e519c11e009b8aec19c408a3adce069f07837679
Tetiana Proshyna <mollinne2@gmail.com>
2021-04-07 16:19:12 +0300

    fix team modal

--- src/js/initModal.js
+++ src/js/initModal.js
@@ -7,6 +7,8 @@ export default function initModal() {
       onCloseEnd: function () {
         const over = document.querySelector('.over');
         over.classList.remove('is-shown');
+        const overTeam = document.querySelector('.team-overlay');
+        overTeam.classList.remove('is-shown-team');
       },
     };
     M.Modal.init(elems, options);
@@ -26,5 +28,11 @@ export default function initModal() {
     if (event.target.nodeName === 'UL') return;
     const over = document.querySelector('.over');
     over.classList.add('is-shown');
+  });
+  const modalTeam = document.querySelector('[data-target="modal-team"]');
+
+  modalTeam.addEventListener('click', () => {
+    const overTeam = document.querySelector('.team-overlay');
+    overTeam.classList.add('is-shown-team');
   });
 }
